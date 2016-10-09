1b7851603c8b1f6329477b5835b13116f93dca8f
rabidima <dmitrirabinovich@Dmitris-MacBook-Pro.local>
2016-10-09 16:10:09 -0700

    adding files from old burger

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name ) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name ) VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name ) VALUES ('Hamburger Steak');
